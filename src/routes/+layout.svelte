32f583594e24509aa1cfb67f4757b232e04031a9
Martin Dennhardt <martin@dennhardt.de>
2023-12-10 12:28:12 +0000

    add all the icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,14 +13,14 @@
 
 <AppShell>
 	<svelte:fragment slot="header">
-		<AppBar>
+		<AppBar padding="p-0" background="bg-primary-500">
 			<svelte:fragment slot="lead">
-				<img src="/ccb_logo.svg" class="h-24" alt="C.C.Buchner Logo" />
+				<img src="/ccb_logo.svg" class="h-20" alt="C.C.Buchner Logo" />
 			</svelte:fragment>
-			<h1 class="text-4xl font-thin">Preisrechner</h1>
+			<h1 class="text-xl text-white">click & study Preisrechner</h1>
 		</AppBar>
 	</svelte:fragment>
-	<div class="container mx-auto my-2">
+	<div class="container mx-auto my-2 px-1">
 		<slot />
 	</div>
 </AppShell>
